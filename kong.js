a19aa0eae20775674858d69f4768c6604f82be58
Shockham <samuelhockham@Samuels-MacBook-Pro.local>
2013-03-21 20:30:47 +0000

    added some semicolons

--- kong.js
+++ kong.js
@@ -9,13 +9,13 @@ var Entity = function(x, y, img, width, height, layer){
 	this.Type = ""; //for determing what the entity is
 	this.Alive = true;
 	this.Img = new Image();
-	this.Img.onload = function(){ this.Ready = true; }
+	this.Img.onload = function(){ this.Ready = true; };
 	this.Img.src = img;
 	this.Rotation = 0;
 	this.layer = layer || 0;
-}
+};
 //Empty update function to start with
-Entity.prototype.update = function(modifier){}
+Entity.prototype.update = function(modifier){};
 //drawing the entity to canvas
 Entity.prototype.draw = function(ctx){
 	ctx.save();
@@ -24,7 +24,7 @@ Entity.prototype.draw = function(ctx){
 	if(this.Img.Ready)
 		ctx.drawImage(this.Img, -(this.Width/2), -(this.Height/2), this.Width, this.Height);
 	ctx.restore();
-}
+};
 //--Textity--
 //constructor
 var Textity = function(x, y, txt, colour, size, layer){
@@ -35,15 +35,15 @@ var Textity = function(x, y, txt, colour, size, layer){
 	this.Size = size;
 	this.Alive = true;
 	this.layer = layer || 0;
-}
+};
 //Empty update function
-Textity.prototype.update = function(modifier){}
+Textity.prototype.update = function(modifier){};
 //draw function
 Textity.prototype.draw = function(ctx){
 	ctx.font= this.Size + "px 'VT323'";
 	ctx.fillStyle = this.Colour;
 	ctx.fillText(this.Text, this.x, this.y);
-}
+};
 //--Rectity--
 //constructor
 var Rectity = function(x, y, colour, width, height, layer){
@@ -54,14 +54,14 @@ var Rectity = function(x, y, colour, width, height, layer){
 	this.Colour = colour;
 	this.Alive = true;
 	this.layer = layer || 0;
-}
+};
 //Empty update
-Rectity.prototype.update = function(modifier){}
+Rectity.prototype.update = function(modifier){};
 //draw function
 Rectity.prototype.draw = function(ctx){
 	ctx.fillStyle = this.Colour;
-  	ctx.fillRect(this.x, this.y, this.Width, this.Height);
-}
+    ctx.fillRect(this.x, this.y, this.Width, this.Height);
+};
 //--Linety-
 //constructor
 var Linity = function(x, y, endX, endY, width, layer){
@@ -73,9 +73,9 @@ var Linity = function(x, y, endX, endY, width, layer){
 	this.Colour = "#000000";
 	this.Alive = true;
 	this.layer = layer || 0;
-}
+};
 //Empty update
-Linity.prototype.update = function(modifier){}
+Linity.prototype.update = function(modifier){};
 //draw function
 Linity.prototype.draw = function(ctx){
 	ctx.save();
@@ -86,27 +86,27 @@ Linity.prototype.draw = function(ctx){
 	ctx.lineWidth = this.Width;
 	ctx.stroke();
 	ctx.restore();
-}
+};
 //--state--
 //constructor
 var State = function(){
-	this.Objects = new Array();
-}
+	this.Objects = [];
+};
 //function to add items to game
 State.prototype.add = function(obj){
 	this.Objects[this.Objects.length] = obj;
-}
+};
 //function to remove items from game
 State.prototype.remove = function(obj){
 	this.Objects.splice(this.Objects.indexOf(obj),1);
-}
+};
 //by default calls all the objects update methods
-State.prototype.update = function(modifier){ 
-	for(obj in this.Objects){
+State.prototype.update = function(modifier){
+	for(var obj in this.Objects){
 		if(this.Objects[obj].Alive)
 			this.Objects[obj].update(modifier);
 	}
-}
+};
 //handles the drawing of everything added to game
 State.prototype.draw = function(){
 	var ctx = [];
@@ -114,11 +114,11 @@ State.prototype.draw = function(){
 		ctx.push(document.getElementById("game_canvas_"+i).getContext("2d"));
 		ctx[i].clearRect(0,0, ctx[i].canvas.width, ctx[i].canvas.height);
 	}
-	for(obj in this.Objects){
+	for(var obj in this.Objects){
 		if(this.Objects[obj].Alive)
 			this.Objects[obj].draw(ctx[this.Objects[obj].layer]);
 	}
-}
+};
 //--Game--
 //contructor
 var Game = function(resx, resy, sizex, sizey){
@@ -127,9 +127,9 @@ var Game = function(resx, resy, sizex, sizey){
 	this.height = resy;
 	for (var i = 0; i <= 2; i++) {
 		this.add_canvas(i, resx, resy, sizex, sizey);
-	};
+	}
 	this.State = new State();
-}
+};
 //function to add canvases or layers
 Game.prototype.add_canvas = function(layer_no, resx, resy, sizex, sizey){
 	var cnv = document.createElement("canvas");
@@ -144,17 +144,17 @@ Game.prototype.add_canvas = function(layer_no, resx, resy, sizex, sizey){
 	var context = cnv.getContext("2d");
 	context.font = '40px Monofett';
     context.textBaseline = 'top';
-	document.body.appendChild(cnv);	
-}
+	document.body.appendChild(cnv);
+};
 
 //by default calls all the objects update methods
-Game.prototype.update = function(modifier){ 
+Game.prototype.update = function(modifier){
 	this.State.update(modifier);
-}
+};
 //handles the drawing of everything added to game
 Game.prototype.draw = function(){
 	this.State.draw();
-}
+};
 // shim layer with setTimeout fallback
 window.requestAnimFrame = (function(){
   return  window.requestAnimationFrame       ||
@@ -176,27 +176,24 @@ Game.prototype.main = function (g) {
     then = now;
     if (g) _g_ = g;
     requestAnimFrame(function(){_g_.main();});
-}
+};
 //called to start the game
 var then = Date.now();
 Game.prototype.start = function(){
 	//starting the playState loop
 	then = Date.now();//so playState knows when it og started
 	this.main(this);
-}
+};
 //function to test if two objects are touching
 Game.prototype.touching = function (a, b){
 	if (
-        a.x <= (b.x + b.Width)
-        && b.x <= (a.x + a.Width)
-        && a.y <= (b.y + b.Height)
-        && b.y <= (a.y + a.Height)
+        a.x <= (b.x + b.Width) && b.x <= (a.x + a.Width) && a.y <= (b.y + b.Height) && b.y <= (a.y + a.Height)
     ) {
         return true;
     }else{
-    	return false;
+        return false;
     }
-}
+};
 //--music--
 //constructor
 var Music = function(file){
@@ -204,18 +201,18 @@ var Music = function(file){
     this.music.src = file;
     this.music2 = document.createElement("audio");
     this.music2.src = file;
-}
+};
 //start a single playthrough
 Music.prototype.play = function(){
 	this.music.play();
-}
+};
 //start the looping
 Music.prototype.loop = function(length){
 	var m = this;
     this.music.play();
     setInterval(function(){m.music.play();}, length);
 	setInterval(function(){m.music2.play();}, length);
-}
+};
 
 // Handle keyboard controls
 var keysDown = {};
